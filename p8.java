82ff980b70da0e933616658e84c865594db73b18
veertank <162274579+veertank@users.noreply.github.com>
2025-04-06 14:24:25 +0530

    Update p8.java

--- p8.java
+++ p8.java
@@ -1,25 +1,12 @@
-public class Rectangle {
-    
-    private double height;
-    private double width;
-    
-    public Rectangle(double height, double width) {
-        this.height = height;
-        this.width = width;
-    }
- 
-    public double computeArea() {
-        return height * width;
-    }
+public class Book {
+    int pages;
 
-    public void displayRectangleDetails() {
-        System.out.println("Height of the rectangle: " + height);
-        System.out.println("Width of the rectangle: " + width);
-        System.out.println("Area of the rectangle: " + computeArea());
+    public Book(int pages) {
+        this.pages = pages;
     }
 
     public static void main(String[] args) {
-        Rectangle myRectangle = new Rectangle(5.0, 7.0);
-        myRectangle.displayRectangleDetails();
+        Book myBook = new Book(300);
+        System.out.println("Number of pages = " + myBook.pages);
     }
 }
