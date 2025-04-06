5380f7d55ccfb344ef0913a82f2124d8fb87dddb
veertank <162274579+veertank@users.noreply.github.com>
2025-04-06 14:09:45 +0530

    Create p2.java

---
+++ p2.java
@@ -0,0 +1,17 @@
+
+public class tank {
+    public static void main(String[]args){
+        int a=1;
+        int b=9;
+        int c=8;
+
+        if (a>b && a>c){
+            System.out.println("max is : "+a);
+        }else if (b>a && b>c){
+            System.out.println("max is : "+b);
+        }else{
+            System.out.println("max is : "+c);
+        }
+
+    }
+}
